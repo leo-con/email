7491151f5cf0956faf30873b03fc8b6fcc4eeb5b
leo-con <56977073+leo-con@users.noreply.github.com>
2020-03-13 13:26:23 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -51,18 +51,27 @@ Motivo: Se Comunica Por Un Tema Personal/Laboral.<br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
+var Vacantes_body = html_parte1 + `
+Nombre: {{Nombre}}<br>
+Motivo: Se Comunica Con El Motivo De Darle Seguimiento A La Vacante De<br>
+ Favor De Comunicarse Lo Más Pronto Posible Al:<br>
+ Teléfono {{Telefono}}<br>
+ O Email {{email}}.<br>
+ConversationID: {{voiceInteractionID}} <br>
+` + html_parte2;
+
+
 var emailTemplates = [
     { id:"COTIZAR EVENTO", subject:"Cotizar Evento", body: Cotizar_Evento_body },
     { id:"PORTAL PROVEEDORES", subject:"Proveedores", body: Proveedores_body},
-    { id:"Gastos De Viaje", subject:"Gastos De Viaje", body: Gastos_De_Viaje_body },
+    { id:"EXPINS", subject:"Gastos De Viaje", body: Gastos_De_Viaje_body },
     { id:"REFERENCIA LABORAL", subject:"Referencias Laborales", body: Referencias_Laborales_body },
     { id:"PERSONAL", subject:"Personal / Laboral", body: Personal_Laboral_body},
-    { id:"LABORAL", subject:"Personal / Laboral", body: Personal_Laboral_body}
+    { id:"LABORAL", subject:"Personal / Laboral", body: Personal_Laboral_body},
+    { id:"VACANTE", subject:"Personal / Laboral", body: Vacantes_body}
 ];
 
-/*
+/*  FALTAN ESTAS QUE ESTAN EN EL SCRIPT
 COMERCIAL
-VACANTE
 OFRECER SERVICIO
-EXPINS
 OTRO*/
