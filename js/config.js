ea96f4433bf11080e2835416196c8fb27886e757
leo-con <56977073+leo-con@users.noreply.github.com>
2020-03-16 11:51:58 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,16 +5,16 @@ Nombre: {{Nombre}}<br>
 Motivo: Solicita Información Acerca De Los Costos De La Cerveza Para Un Evento Personal. <br>
 Favor De Comunicarse Lo Más Pronto Posible Al: <br>
 Teléfono: {{Telefono}}<br>
- O Email {{email}} <br>
+O Email {{email}} <br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
 var Proveedores_body = html_parte1 + `
 Nombre: {{Nombre}}<br>
-Numero Proveedor: {{proveedor}}<br>
-Folios De Las Facturas: {{foliosFacturas}}<br>
-Estatus: {{status}}<br>
-Fecha De Recepción: ( )<br>
+Numero Proveedor: {{numero_prov}}<br>
+Folios De Las Facturas: {{folios}}<br>
+Estatus: {{status_proveedores}}<br>
+Fecha De Recepción: {{fecha_recep}}<br>
 Motivo: Solicita Apoyo Con Unas Factura Pendientes De Pago.<br>
 Favor De Comunicarse Lo Más Pronto Posible Al:<br>
 Teléfono: {{Telefono}}<br>
@@ -27,51 +27,46 @@ var Gastos_De_Viaje_body = html_parte1 + `
 Nombre: {{Nombre}}<br>
 Motivo: Se Comunica Por Un Tema De Gastos De Viaje. <br>
 Favor De Comunicarse Lo Más Pronto Posible Al:<br>
- Teléfono {{Telefono}}<br>
- O Email {{email}}.<br>
+Teléfono {{Telefono}}<br>
+O Email {{email}}.<br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
 var Referencias_Laborales_body = html_parte1 + `
 Nombre: {{Nombre}}<br>
 Empresa: {{Empresa}}<br>
-Motivo: Solicita Referencias Laborales De: {{Motivo}}<br>
- Favor De Comunicarse Lo Más Pronto Posible Al:<br>
- Teléfono {{Telefono}}<br>
- O Email {{email}}.<br>
+Motivo: Solicita Referencias Laborales De: {{referencia_lab}}<br>
+Favor De Comunicarse Lo Más Pronto Posible Al:<br>
+Teléfono {{Telefono}}<br>
+O Email {{email}}.<br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
 var Personal_Laboral_body = html_parte1 + `
 Nombre: {{Nombre}}<br>
 Motivo: Se Comunica Por Un Tema Personal/Laboral.<br>
- Favor De Comunicarse Lo Más Pronto Posible Al:<br>
- Teléfono {{Telefono}}<br>
- O Email {{email}}.<br>
+Favor De Comunicarse Lo Más Pronto Posible Al:<br>
+Teléfono {{Telefono}}<br>
+O Email {{email}}.<br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
 var Vacantes_body = html_parte1 + `
 Nombre: {{Nombre}}<br>
-Motivo: Se Comunica Con El Motivo De Darle Seguimiento A La Vacante De<br>
- Favor De Comunicarse Lo Más Pronto Posible Al:<br>
- Teléfono {{Telefono}}<br>
- O Email {{email}}.<br>
+Motivo: Se Comunica Con El Motivo De Darle Seguimiento A La Vacante De: {{vacante}}<br>
+Favor De Comunicarse Lo Más Pronto Posible Al:<br>
+Teléfono {{Telefono}}<br>
+O Email {{email}}.<br>
 ConversationID: {{voiceInteractionID}} <br>
 ` + html_parte2;
 
 
 var emailTemplates = [
-    { id:"COTIZAR EVENTO", subject:"Cotizar Evento", body: Cotizar_Evento_body },
-    { id:"PORTAL PROVEEDORES", subject:"Proveedores", body: Proveedores_body},
-    { id:"EXPINS", subject:"Gastos De Viaje", body: Gastos_De_Viaje_body },
-    { id:"REFERENCIA LABORAL", subject:"Referencias Laborales", body: Referencias_Laborales_body },
-    { id:"PERSONAL", subject:"Personal / Laboral", body: Personal_Laboral_body},
-    { id:"LABORAL", subject:"Personal / Laboral", body: Personal_Laboral_body},
-    { id:"VACANTE", subject:"Personal / Laboral", body: Vacantes_body}
+    { id:"PERSONAL_LABORAL", subject:"Personal / Laboral", body: Personal_Laboral_body},
+    { id:"COTIZAR_EVENTO", subject:"Cotizar Evento", body: Cotizar_Evento_body },
+    { id:"PORTAL_PROVEEDORES", subject:"Proveedores", body: Proveedores_body},
+    { id:"VACANTE", subject:"Personal / Laboral", body: Vacantes_body},
+    { id:"REFERENCIA_LABORAL", subject:"Referencias Laborales", body: Referencias_Laborales_body },
+    { id:"GASTOS_DE_VIAJE", subject:"Gastos De Viaje", body: Gastos_De_Viaje_body }
 ];
 
-/*  FALTAN ESTAS QUE ESTAN EN EL SCRIPT
-COMERCIAL
-OFRECER SERVICIO
-OTRO*/
